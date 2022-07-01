a73cc4f22094307eac69d887eada9337025bf51a
Zen <muhzaini30@gmail.com>
2022-07-01 08:10:25 +0800

    upload dulu

--- app/src/main/assets/src/pages/index.vue
+++ app/src/main/assets/src/pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="bagianAtas h-[50vh]"></div>
+  <div class="bagianAtas content-end grid h-[60vh]">
+    <div class="tampil text-xl bg-red-100 grid grid-cols-5">
+      <button class="col-span-1">-</button>
+      <div class="col-span-3 text-right px-2">1.000 x 1</div>
+      <button class="col-span-1">+</button>
+    </div>
+    <div class="tampil text-xl bg-red-100 grid grid-cols-5">
+      <button class="col-span-1">-</button>
+      <div class="col-span-3 text-right px-2">26.000.000 x 2</div>
+      <button class="col-span-1">+</button>
+    </div>
+  </div>
   <div
-    class="bagianBawah grid text-2xl grid-cols-4 bg-orange-500 h-[50vh] text-white"
+    class="bagianBawah grid text-2xl grid-cols-4 bg-orange-500 h-[40vh] text-white"
   >
     <button>7</button>
     <button>8</button>
@@ -22,9 +33,15 @@
 
 <style scoped="">
 .bagianBawah button {
-  @apply hover:bg-yellow-500;
+  @apply hover:bg-yellow-500 active:bg-transparent;
 }
 .icon {
   @apply mx-auto;
+}
+.tampil > * {
+  @apply py-2;
+}
+.tampil .col-span-1 {
+  @apply text-center bg-red-200;
 }
 </style>
